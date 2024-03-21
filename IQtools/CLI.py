e50727f5ac59d0e28e5acd3d908e8f20c5e9ff7a
logfaa <logfaa@amazon.com>
2024-03-21 13:04:12 -0700

    Stubbed out spectrum plot function

--- IQtools/CLI.py
+++ IQtools/CLI.py
@@ -32,3 +32,18 @@ def getPeaks(args=None):
     for index, peak, frequency in zip(range(peaksFound), peaks, peaksF):
         print('{index}: {peak:.2f} dBFS @ {frequency: 0.2f} MHz'.format(index=index, peak=peak, frequency=frequency))
 
+def getSpectrum(args=None):
+    parser = argparse.ArgumentParser(description='Utility for generating a frequency spectrum from an IQ data file')
+    parser.add_argument('filename', help='File with IQ data', type=str)
+    parser.add_argument('sampleRate', help='Complex sample rate of the file', type=float)
+    parser.add_argument('-f', help='Frequency Units', type=float, default=1E6)
+    parser.add_argument('-b', help='Number of signed bits for the ADC', type=int, default=12)
+    parser.add_argument('-i', help='Generate a png of the spectrum instead of an ASCII plot', action='store_true')
+
+    config = parser.parse_args(args)
+    bits = config.b
+    sampleRate = config.sampleRate
+    filename = config.filename
+    frequencyUnits = config.f
+    data = IQdata(filename, sampleRate, bits)
+    sa = SignalAnalyzer(data)
